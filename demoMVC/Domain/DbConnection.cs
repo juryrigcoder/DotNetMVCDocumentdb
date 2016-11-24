2f52ccdf4cfd5af3e808e3aeeb6023c23c29101b
Andrew <andrew@laytipster.co.uk>
2016-11-24 09:28:20 +0000

    Initial code commit.

---
+++ demoMVC/Domain/DbConnection.cs
@@ -0,0 +1,9 @@
+﻿using System.Web;
+
+namespace demoMVC.Domain
+{
+    public class DbConnection
+    {
+        public static string DbLocation = HttpContext.Current.Server.MapPath(@"~/Database/projectdb.db");
+    }
+}
